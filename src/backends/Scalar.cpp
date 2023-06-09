01fdd7df8e3e0773e9b3527d620435c554abd018
Truman Kilen <t@kilen.me>
2023-06-08 22:51:58 -0500

    Special case first byte which allows clang optimization

--- src/backends/Scalar.cpp
+++ src/backends/Scalar.cpp
@@ -2,26 +2,24 @@
 
 namespace LightningScanner {
 
-ScanResult FindScalar(const Pattern& patternData, void* startAddr,
-                      size_t size) {
-    uint8_t* binary = (uint8_t*)startAddr;
+ScanResult FindScalar(const Pattern& patternData, void* startAddr, size_t size) {
+    uint8_t* it = (uint8_t*)startAddr;
+    uint8_t* end = it + size - patternData.data.size() + 1;
 
-    for (size_t binaryOffset = 0; binaryOffset < size; ++binaryOffset) {
+    uint8_t needle = patternData.data[0];
+    for (; it != end; it++) {
+        if (*it != needle) continue;
         bool found = true;
-        for (size_t patternOffset = 0; patternOffset < patternData.data.size();
-             patternOffset++) {
-            if (patternData.mask[patternOffset] == 0x00)
-                continue;
-            if (*(binary + binaryOffset + patternOffset) !=
-                patternData.data[patternOffset]) {
+
+        for (size_t patternOffset = 0; patternOffset < patternData.data.size(); patternOffset++) {
+            if ((it[patternOffset] & patternData.mask[patternOffset]) != patternData.data[patternOffset]) {
                 found = false;
                 break;
             }
         }
 
-        if (found) {
-            return ScanResult((void*)(binary + binaryOffset));
-        }
+        if (found)
+            return ScanResult((void*)it);
     }
 
     return ScanResult(nullptr);
